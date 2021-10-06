8843e326473068dba5b4068fb1e916467e0c7fe8
Muhammed Mikayilov <muhammedmikayilov@Muhammed-MacBook-Pro.local>
2021-10-07 00:29:24 +0400

    Classwork

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// camelCase();
+
+// function camelCase(c) {
+//   let a = 5;
+//   console.log("this is ", a);
+
+//   console.log("This is inner func", innerFunc(10, 5));
+
+//   function innerFunc(a, b) {
+//     return a * b;
+//   }
+// }
+
+// let number = 10;
+// let string = "Hello";
+// let numberTwo = 5;
+
+// function withParameters(a, b, c) {
+//   return a + (b + c);
+// }
+// // console.log(test);
+// var test = "string";
+
+// // console.log(func());
+// let func = function (a, b) {
+//   console.log(a + b);
+// };
+
+// let outerFunc = function () {
+
+// }
+// let aTwo = 6;
+
+// let a = function () {
+//   //   console.log("This is func");
+//   definedFunc();
+// };
+
+// // console.log(a());
+// // console.log("This is");
+// a();
+
+// function definedFunc() {
+//   //   console.log("this is defined");
+//   return 5 + 10;
+// }
+
+// let myArrowFunc = (a, b) => {
+//   if (b === 4 && a === 2) {
+//     return "true 2 and 4";
+//   }
+//   if (a === 2) {
+//     return "Hello";
+//   }
+
+//   return false;
+// };
+
+// console.log(myArrowFunc(5, 2));
+
+function userGender(gender) {
+  if (gender === "Male") {
+    return gender;
+  } else if (gender === "Female") {
+    return gender;
+  }
+  return false;
+}
+console.log(userGender("Male"));
